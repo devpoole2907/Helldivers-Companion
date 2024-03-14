953debd1ac90e558157cca516fd2868801bc1d1c
James Poole <jamesdpoole@icloud.com>
2024-03-14 17:54:45 +1300

    Initial Commit

---
+++ Helldivers Companion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Helldivers Companion
+//
+//  Created by James Poole on 14/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Helldivers Companion/Helldivers_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Helldivers_CompanionApp.swift
+//  Helldivers Companion
+//
+//  Created by James Poole on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Helldivers_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
