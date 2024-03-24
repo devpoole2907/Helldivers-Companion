7acbb8111f70a47c79b33602d50e7c2040abdf5d
James Poole <jamesdpoole@icloud.com>
2024-03-24 17:44:37 +1300

    Stratagem Hero iOS/watchOS high score sync improvements - Sends to game center if score is updated on iOS from the watch

--- Helldivers Companion/Model/WatchConnectivityProvider.swift
+++ Helldivers Companion/Model/WatchConnectivityProvider.swift
@@ -7,48 +7,59 @@
 
 import SwiftUI
 import WatchConnectivity
+#if os(iOS)
+import GameKit
+#endif
 
 class WatchConnectivityProvider: NSObject, ObservableObject, WCSessionDelegate {
-    // Called when the session has been activated.
-        func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-            // Handle activation completion, check for errors, etc.
-            if let error = error {
-                print("WC Session activation failed with error: \(error.localizedDescription)")
-                return
-            }
-            print("WC Session activated with state: \(activationState.rawValue)")
-        }
-    #if os(iOS)
-        // iOS only: Called when the session could not be activated or if the session becomes inactive.
-        func sessionDidBecomeInactive(_ session: WCSession) {
-            // Handle session becoming inactive.
-            print("WC Session did become inactive")
-        }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
         
-        // iOS only: Called when the session has been deactivated.
-        func sessionDidDeactivate(_ session: WCSession) {
-            // Handle session deactivation, you may need to activate a new session.
-            print("WC Session did deactivate")
-            session.activate()  // Often you'll want to reactivate the session after it's been deactivated.
+        if let error = error {
+            print("WC Session activation failed with error: \(error.localizedDescription)")
+            return
         }
-    #endif
+        print("WC Session activated with state: \(activationState.rawValue)")
+    }
+#if os(iOS)
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        
+        print("WC Session did become inactive")
+    }
+    
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        
+        print("WC Session did deactivate")
+        session.activate()  // reactivate session if deactivated
+    }
+#endif
     
     // for apple watch to receive the high score from the ios app, update its high score if its higher on ios
     // and vice versa
     func session(_ session: WCSession, didReceiveUserInfo userInfo: [String : Any] = [:]) {
-            DispatchQueue.main.async {
-                if let receivedHighScore = userInfo["highScore"] as? Int {
-                    if receivedHighScore > self.highScore {
-                        self.highScore = receivedHighScore
-                    }
+        DispatchQueue.main.async {
+            if let receivedHighScore = userInfo["highScore"] as? Int {
+                if receivedHighScore > self.highScore {
+                    self.highScore = receivedHighScore
+                    
+#if os(iOS)
+                    // update the game center score with the new one
+                    let gameCenterManager = GameCenterManager()
+                    let leaderboardId = "com.poole.james.helldiverscompanion.highscores"
+                    gameCenterManager.reportScore(score: self.highScore, leaderboardID: leaderboardId)
+#endif
+                    
                 }
             }
         }
+    }
     
     static let shared = WatchConnectivityProvider()
     
     @AppStorage("highScore") var highScore: Int = 0
-
+    
     override init() {
         super.init()
         if WCSession.isSupported() {
@@ -57,11 +68,11 @@ class WatchConnectivityProvider: NSObject, ObservableObject, WCSessionDelegate {
             session.activate()
         }
     }
-    
+    // uses transfer user info not send message, in order to queue the sent data if the app on the device to send to is inactive
     func sendHighScore(highScore: Int) {
         let userInfo = ["highScore": highScore]
         WCSession.default.transferUserInfo(userInfo)
     }
-
-
+    
+    
 }
